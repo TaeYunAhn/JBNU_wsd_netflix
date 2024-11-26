cb1e23cd9d214d5dda0d8575527a986139dc873f
TaeYunAhn <zzz6874@naver.com>
2024-11-26 09:15:11 +0900

    add : src/

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Popular from './pages/Popular';
+import Search from './pages/Search';
+import Wishlist from './pages/Wishlist';
+import SignIn from './pages/SignIn';
+import MovieDetails from './pages/MovieDetails';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import ScrollToTop from './components/ScrollToTop'; // ScrollToTop imported
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <ScrollToTop /> {/* ScrollToTop added here */}
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/popular"
+          element={
+            <PrivateRoute>
+              <Popular />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <PrivateRoute>
+              <Search />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/wishlist"
+          element={
+            <PrivateRoute>
+              <Wishlist />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/movie/:id"
+          element={
+            <PrivateRoute>
+              <MovieDetails />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
